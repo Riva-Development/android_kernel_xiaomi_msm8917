e1442d11a9b88c1dd8a271675ba01f6d4dd9508d
Malpa73 <kmktoczek@gmail.com>
2018-05-20 11:12:17 +0200

    repairing fucking hoptlug.

--- arch/arm64/kernel/alucard_hotplug.c
+++ arch/arm64/kernel/alucard_hotplug.c
@@ -69,8 +69,8 @@ static struct hotplug_tuners {
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 65,
 	.hotplug_enable = 1,
-	.min_cpus_online = 1,
-	.maxcoreslimit = 4,
+	.min_cpus_online = 2,
+	.maxcoreslimit = 2,
 	.maxcoreslimit_sleep = 1,
 	.hp_io_is_busy = 0,
 #if defined(CONFIG_POWERSUSPEND) || \
